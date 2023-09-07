54db1ec4879cd76b35c1d936cbd35070e0698d89
[Hwang jaeuk] <ghkdwodnr123@gmail.com>
2023-09-07 23:15:22 +0900

    selenum add

--- 착한 기업.py
+++ 착한 기업.py
@@ -1,10 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
+import selenium
 
 #url , 지정 정보
 url = "https://www.goodprice.go.kr/search/goodstore.do"
 data={"OptSido":"14", "OptGugun":"구미시"}
 
+#드라이버 지정
+dr = webdriver.Chrome()
+dr.get("https://www.goodprice.go.kr/search/goodstore.do")
 #request(POST)
 res = requests.post(url, data)
 
@@ -14,4 +18,9 @@ soup = BeautifulSoup(res.content, "html.parser")
 #crwling
 actor_list = soup.find_all("td", {'class':["board_seven"]})
 
-print(actor_list)
+
+if res.status_code == 200:
+
+    print(actor_list)
+else :
+    print("오류 코드 :", res.status_code)
