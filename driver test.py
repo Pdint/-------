b29aa1c9e5f0ec584af752ae59222cff23ace9e7
[Hwang jaeuk] <ghkdwodnr123@gmail.com>
2023-09-08 11:14:35 +0900

    add selenium

--- driver test.py
+++ driver test.py
@@ -15,11 +15,11 @@ dr = webdriver.Chrome()
 dr.get("https://www.goodprice.go.kr/search/goodstore.do")
 
 #태그 확인
-#try:
-    #element = WebDriverWait(dr, 3).utill(Ec.presence_of_element_located(By.CLASS_NAME, ))
 
-name = find_elements_by_class_name('board_three')
-address = find_elements_bt_class_name('board_seven')
-call = find_elements_bt_class_name('board_six')
-goods = find_elements_bt_class_name('board_four')
-prize = find_elements_bt_class_name('board_five')
+for i in range(1, pageNum):
+
+    ame = find_elements_by_class_name('board_three')
+    address = find_elements_bt_class_name('board_seven')
+    call = find_elements_bt_class_name('board_six')
+    goods = find_elements_bt_class_name('board_four')
+    prize = find_elements_bt_class_name('board_five')
--- 착한 기업.py
+++ 착한 기업.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+import time
 #url , 지정 정보
 url = "https://www.goodprice.go.kr/search/goodstore.do"
 data={"OptSido":"14", "OptGugun":"구미시"}
@@ -11,6 +11,15 @@ data={"OptSido":"14", "OptGugun":"구미시"}
 dr = webdriver.Chrome()
 dr.get(url)
 
+#클래스 요소 클릭하기
+num = 4
+for i in range(5):
+    elements = dr.find_elements (By.XPATH, '//*[@id="cont-sbj"]/form[2]/div[2]/a[' + str(num) + ']')
+    for element in elements:
+        element.click()
+        num += 1
+        time.sleep(10)
+
 #request(POST)
 res = requests.post(url, data)
 
@@ -18,11 +27,14 @@ res = requests.post(url, data)
 soup = BeautifulSoup(res.content, "html.parser")
 
 #crwling
-actor_list = soup.find_all("td", {'class':["board_seven"]})
+actor_list = soup.find_all("td")
 
 
 if res.status_code == 200:
 
-    print(actor_list)
+    with open("all.txt", "a", encoding="utf-8") as file:
+       for actor in actor_list:
+            file.write(str(actor) + "\n")
+
 else :
     print("오류 코드 :", res.status_code)
