6022f65aa6194d4fd40821b00b9bb0882735cbbd
[Hwang jaeuk] <ghkdwodnr123@gmail.com>
2023-09-09 02:09:01 +0900

    요소 선택 오류 버전

--- 착한 기업.py
+++ 착한 기업.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+
+
 #url , 지정 정보
 url = "https://www.goodprice.go.kr/search/goodstore.do"
 data={"OptSido":"14", "OptGugun":"구미시"}
@@ -11,20 +13,37 @@ data={"OptSido":"14", "OptGugun":"구미시"}
 dr = webdriver.Chrome()
 dr.get(url)
 
-#클래스 요소 클릭하기
-num = 4
-for i in range(5):
-    elements = dr.find_elements (By.XPATH, '//*[@id="cont-sbj"]/form[2]/div[2]/a[' + str(num) + ']')
-    for element in elements:
-        element.click()
-        num += 1
-        time.sleep(10)
+# 데이터를 POST 방식으로 서버에 전송할 수 있는 웹 페이지로 이동합니다.
+dr = webdriver.Chrome()
+dr.get(url)
+
+# 데이터 입력
+opt_sido = dr.find_element(By.NAME, "OptSido")
+opt_gugun = dr.find_element(By.NAME, "OptGugun")
+
+opt_sido.send_keys("14")  # 시도 입력
+opt_gugun.send_keys("구미시")  # 구군 입력
+
+# 데이터 전송 (폼 제출)
+opt_gugun.send_keys(Keys.RETURN)
+
 
 #request(POST)
 res = requests.post(url, data)
 
 #soup
 soup = BeautifulSoup(res.content, "html.parser")
+#클래스 요소 클릭하기
+
+
+num = 4
+for i in range(5):
+    elements = dr.find_elements (By.XPATH, '//*[@id="cont-sbj"]/form[2]/div[2]/a[' + str(num) + ']')
+    for element in elements:
+        element.click()
+        num += 1
+
+        time.sleep(10)
 
 #crwling
 actor_list = soup.find_all("td")
