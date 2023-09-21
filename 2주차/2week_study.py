6e8f6f959de8fe1f2c5331f293b94cd46ff176a6
Pdint <ghkdwodnr123@gmail.com>
2023-09-21 22:31:04 +0900

    최종

--- 2주차/2week_study.py
+++ 2주차/2week_study.py
@@ -1,10 +1,13 @@
 import selenium
-from bs4 import BeautifulSoup
+from wordcloud import WordCloud
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from collections import Counter
 import re
 import time
 from konlpy.tag import Komoran
+import matplotlib.pyplot as plt
+
 ### 구글 뉴스 제목 크롤링
 dr = webdriver.Chrome()
 url = 'https://news.google.com/home?hl=ko&gl=KR&ceid=KR:ko'
@@ -28,22 +31,30 @@ news  = news.replace('...', ' ') #... 제거
 pattern = r'[^a-zA-Z가-힣]' #특수 문자 제거
 news = re.sub(pattern=pattern, repl=' ', string=news)
 
-pattern = r'\b(?:SBS|MBC|KBS|JTBC|연합)\s*뉴스\b' 
+pattern = r'\b(?:SBS|MBC|KBS|JTBC|연합)\s*뉴스\b'
 news = re.sub(pattern=pattern, repl=" ", string=news)
 
 pattern = r'\b(?:SBS|MBC|KBS|JTBC|연합)\s*News\b' 
 news = re.sub(pattern=pattern, repl=" ", string=news)
 
-pattern = r'\b(?:SBS|MBC|KBS|JTBC|연합)\b'
+pattern = r'\b(?:SBS|MBC|KBS|JTBC|연합|뉴스A)\b'
 news = re.sub(pattern=pattern, repl=" ", string=news)
 
 ##############
-komoran = Komoran(userdic="user.dic")
+komoran = Komoran()
 komoran_tag = komoran.pos(news)
 
-words = [word for word, pos in komoran_tag if pos == 'NNP' or pos == 'SL']
+words = [word for word, pos in komoran_tag if (pos == 'NNP' or pos == 'SL') and len(word) > 1]
+c = Counter(words)
+
+wc = WordCloud(font_path='gulim', width=400, height=400, scale=2.0, max_font_size=250)
+gen = wc.generate_from_frequencies(c)
+plt.figure()
+plt.imshow(gen)
+
+wc.to_file('워드클라우드.png')
 
 #print(komoran_tag)
-print(words)
+#
 #print(news)
-
+#print(words)
